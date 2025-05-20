dbe83c69aacc7e03aa08caea95e27c76a368d421
root <root@LAPTOP-CIFFJCB4>
2025-05-20 13:42:53 +0200

    revamped 4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,14 @@
 const http = require('http');
 
+const port = 1245;
+
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-type', 'text/plain');
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
   res.end('Hello Holberton School!');
 });
-app.listen = 1245;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
 module.exports = app;
