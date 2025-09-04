f103329937a100bfe27c241f6d7a2db678e479b8
root <root@LAPTOP-CIFFJCB4>
2025-09-04 09:20:21 +0200

    changed grammar

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,5 @@
-// displayMessage function prints a message
-function displayMessage(message) {
+// Use displayMessage function to print a message to the console.
+const displayMessage = (message) => {
   console.log(message);
-}
-
+};
 module.exports = displayMessage;
